880533dbe4f8eb0cad77dd5b1bf62a91bb005ab1
Fuzuli635 <fuzulieliyev213@gmail.com>
2024-08-23 10:15:03 +0400

    Created Next.js app

---
+++ weather-app/src/app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+type Props = {};
+
+export default function Navbar({}: Props) {
+  return (
+    <nav className="shadow-sm top-0 left-0 sticky z-50 bg-white ">
+      <div className="h-[80px]     w-full    flex   justify-between items-center  max-w-7xl px-3 mx-auto">
+        
+      </div>
+      Navbar
+    </nav>
+  );
+}
---
+++ weather-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "./components/Navbar";
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello World</h1>
+    </div>
+  );
+}
